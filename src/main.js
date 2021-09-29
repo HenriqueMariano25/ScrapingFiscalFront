be02d1da980daa9ac15db6d95bc3596e2025df81
HenriqueMariano25 <henrique.mariano25@hotmail.com>
2021-09-29 09:18:38 -0300

    Cria leitor de QR COde

--- src/main.js
+++ src/main.js
@@ -2,12 +2,17 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './routes'
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import VModal from 'vue-js-modal'
+import VueQrcodeReader from "vue-qrcode-reader";
+
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.use(BootstrapVue)
 Vue.use(IconsPlugin)
+Vue.use(VModal)
+Vue.use(VueQrcodeReader);
 
 Vue.config.productionTip = false
 
